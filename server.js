f18c19b9e670add4aa96f0dc269a3f2d91ab20a9
Mohammad_Mohammad <mm6dd.mh@gmail.com>
2023-05-07 12:48:51 +0100

    alot of changes lol

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express()
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res)=> {
+    res.send('test')
+})
+
 app.listen(PORT, ()=> {
     console.log(`Server is listening on PORT ${PORT}`)
 })
